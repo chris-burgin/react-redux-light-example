520d83058d1c2fa9987551ad1403a42ee9288005
ChrisBurgin <chrisburgin95@gmail.com>
2017-09-02 20:28:29 -0400

    Enabled Redux Dev Tools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,6 +2,9 @@ import { createStore } from "redux"
 import rootReducer from "../reducers"
 
 const configureStore = initalState =>
-  createStore(rootReducer, initalState)
+  createStore(
+    rootReducer, 
+    initalState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 export default configureStore
